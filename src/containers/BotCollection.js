586d3afe6c75b043c8b520249d883e0b8a6e4c92
Tisdale511 <tisdalefry@gmail.com>
2020-08-11 13:06:12 -0700

    sorry.

--- src/containers/BotCollection.js
+++ src/containers/BotCollection.js
@@ -4,11 +4,14 @@ class BotCollection extends Component {
   //your code here
 
   render() {
+    // let {id, name, health, damage, armor, bot_class, catchphrase, avatar_url, created_at, updated_at} = this.props.bots
+    // let allBots = (props.bots.map(bots => bots={bots}))
     return (
       <div className="ui four column grid">
         <div className="row">
-          {/*...and here..*/}
+          
           Collection of all bots
+          {this.props.bots.map(bot => `<div> <card value=${bot} /></div>)`)}
         </div>
       </div>
     );
