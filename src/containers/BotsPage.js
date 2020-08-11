0dd4632562d7f474cf0bc346d6eefeac66d37f1e
sfespinosa <scottjames.espinosa@gmail.com>
2020-08-11 11:19:08 -0700

    added bot sort

--- src/containers/BotsPage.js
+++ src/containers/BotsPage.js
@@ -1,19 +1,21 @@
 import React, { Component } from "react";
 import BotCollection from "./BotCollection";
 import YourBotArmy from "./YourBotArmy";
+import SortBar from "./SortBar";
 
 class BotsPage extends Component {
   //start here with your code for step one
   state = {
     bots: [],
-    botArmy: []
+    botArmy: [],
+    sortByMethod: ""
   }
 
   componentDidMount(){
     fetch('http://localhost:6001/bots')
     .then(res => res.json())
     .then(json => this.setState({
-      bots: json
+      bots: json,
     }))
   }
 
@@ -45,9 +47,23 @@ class BotsPage extends Component {
     }
   }
 
+  handleSort = (e) =>{
+    if (e.target.value === 'health') {
+      let bots = this.state.bots.sort((a,b) => a.health - b.health)
+      this.setState({ bots })
+    } else if (e.target.value === 'damage') {
+      let bots = this.state.bots.sort((a,b) => a.damage - b.damage)
+      this.setState({ bots })
+    } else if (e.target.value === 'armor') {
+      let bots = this.state.bots.sort((a,b) => a.armor - b.armor)
+      this.setState({ bots })
+    }
+  }
+
   render() {
     return <div>
       <YourBotArmy botArmy={this.state.botArmy} removeFromBotArmy={this.removeFromBotArmy}/>
+      <SortBar handleSort={this.handleSort} sortByMethod={this.state.sortByMethod}/>
       <BotCollection bots={this.state.bots} addToBotArmy={this.addToBotArmy} dischargeBot={this.dischargeBot}/>
       </div>;
   }
